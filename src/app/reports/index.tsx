b5f0bc82c957f4fc6e4504f58aa9f89204b961d6
Cristian Narvaez <crissnar12xd@gmail.com>
2025-06-16 12:38:33 -0500

    [FIX] fixed report permissions

--- src/app/reports/index.tsx
+++ src/app/reports/index.tsx
@@ -7,14 +7,13 @@ export const ReporteProyectos = () => {
       <div className="flex w-full items-center justify-center flex-col gap-2">
         <iframe
           allowFullScreen={true}
-          className=""
           frameBorder="0"
           height="700"
-          src="https://app.powerbi.com/reportEmbed?reportId=41cc3973-1378-4915-bb6f-9eb6988405df&autoAuth=true&ctid=cbc2c381-2f2e-4d93-91d1-506c9316ace7"
+          src="https://app.powerbi.com/view?r=eyJrIjoiMDFjNjUzMTAtMjdhOS00YTQ2LTk0MmUtMzRmNDc2MTY2ZmI2IiwidCI6ImNiYzJjMzgxLTJmMmUtNGQ5My05MWQxLTUwNmM5MzE2YWNlNyIsImMiOjR9"
           title="Seguimiento Innovación y CompetitividadWEB"
           width="100%"
-        />
-        <Link href="https://app.powerbi.com/reportEmbed?reportId=41cc3973-1378-4915-bb6f-9eb6988405df&autoAuth=true&ctid=cbc2c381-2f2e-4d93-91d1-506c9316ace7">
+         />
+        <Link href="https://app.powerbi.com/view?r=eyJrIjoiMDFjNjUzMTAtMjdhOS00YTQ2LTk0MmUtMzRmNDc2MTY2ZmI2IiwidCI6ImNiYzJjMzgxLTJmMmUtNGQ5My05MWQxLTUwNmM5MzE2YWNlNyIsImMiOjR9">
           <p className="underline">
             <strong>Ver en Power BI</strong>
           </p>
